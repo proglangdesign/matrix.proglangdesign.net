866c8ddff0b8f00eb6a5e33d60f06d7ef774257d
James Martin <james@jtmar.me>
2019-01-10 15:00:50 -0800

    Publishing final configuration before opening bridge!

--- export-config.sh
+++ export-config.sh
@@ -11,3 +11,4 @@ REGISTRATION_SHARED_SECRET=`cat $SECRETS_PATH/registrationSharedSecret`
 sed "s/\\\\discordBotToken/$DISCORD_BOT_TOKEN/g; s@\\\\data@$DATA_PATH@g" $CONFIG_PATH/matrix-appservice-discord/config.yaml > $CONFIG_PATH/matrix-appservice-discord/config.yaml.real
 sed "s@\\\\data@$DATA_PATH@g; s/\\\\ircBotPassword/$IRC_BOT_PASSWORD/g; s@\\\\secrets@$SECRETS_PATH@g" $CONFIG_PATH/matrix-appservice-irc/config.yaml > $CONFIG_PATH/matrix-appservice-irc/config.yaml.real
 sed "s@\\\\data@$DATA_PATH@g; s@\\\\secrets@$SECRETS_PATH@g; s@\\\\software@$SOFTWARE_PATH@g; s@\\\\config@$CONFIG_PATH@g; s/\\\\registrationSharedSecret/$REGISTRATION_SHARED_SECRET/g; s/\\\\passwordPepper/$PASSWORD_PEPPER/g" $CONFIG_PATH/matrix-synapse/homeserver.yaml > $CONFIG_PATH/matrix-synapse/homeserver.yaml.real
+cp $CONFIG_PATH/matrix-appservice-discord/config.yaml.real $SOFTWARE_PATH/matrix-appservice-discord/config.yaml
