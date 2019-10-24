50674ce8aa3354ded76fa2b652b92170a76e87a7
James Martin <james@jtmar.me>
2019-10-23 22:22:37 -0700

    Removed support for riot, updated upstream IRC bridge

--- setup-nginx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ROOT_PATH=`pwd`
-sudo ln -s $ROOT_PATH/config/nginx/sites-available/matrix.qipl.org /etc/nginx/sites-enabled/
-sudo service nginx reload
--- setup-riot-web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-ROOT_PATH=`pwd`
-CONFIG_PATH="$ROOT_PATH/config"
-cd software/riot-web
-npm install
-npm run build
-sudo ln -s $ROOT_PATH/software/riot-web/webapp /opt/riot
-sudo cp $CONFIG_PATH/riot-web/config.json /opt/riot
