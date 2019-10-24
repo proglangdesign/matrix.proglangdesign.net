2d0edf3d82fb6b75a20c22aea79145ec0a658da2
James Martin <james@jtmar.me>
2019-10-24 08:37:22 +0000

    Moved service to a new host.

--- run-matrix-appservice-irc.sh
+++ run-matrix-appservice-irc.sh
@@ -2,4 +2,7 @@
 ROOT_PATH=`pwd`
 CONFIG_PATH="$ROOT_PATH/config"
 cd software/matrix-appservice-irc
-node app.js -c $CONFIG_PATH/matrix-appservice-irc/config.yaml.real -f "$CONFIG_PATH/matrix-appservice-irc/registration.yaml" -p 9999
+# sudo is necessary to bind to the ident port (113).
+# obviously this is a terrible idea,
+# but I don't feel like figuring out a workaround right now.
+sudo node app.js -c $CONFIG_PATH/matrix-appservice-irc/config.yaml.real -f "$CONFIG_PATH/matrix-appservice-irc/registration.yaml" -p 9999
