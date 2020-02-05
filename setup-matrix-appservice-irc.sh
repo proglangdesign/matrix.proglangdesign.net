df5640dbb0cac52a6aab5c656012c486f4f368aa
James Martin <james@jtmar.me>
2020-02-05 22:48:18 +0000

    Updated matrix-appservice-irc to 0.15.

--- setup-matrix-appservice-irc.sh
+++ setup-matrix-appservice-irc.sh
@@ -2,5 +2,5 @@
 ROOT_PATH=`pwd`
 CONFIG_PATH="$ROOT_PATH/config"
 cd software/matrix-appservice-irc
-npm install
+yarn
 node app.js -r -f $CONFIG_PATH/matrix-appservice-irc/registration.yaml -u "http://localhost:9999" -c $CONFIG_PATH/matrix-appservice-irc/config.yaml.real -l irc_bot
